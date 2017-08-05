48d62f786d5355a4f0ba02c8f281e146af43994c
swiknaba <ludwig.reinmiedl@gmail.com>
2017-08-06 01:39:05 +0200

    done

--- js/app.js
+++ js/app.js
@@ -65,63 +65,141 @@ function initMap() {
 
 
 
-// basic data for example-data
+//example-data, all in Berlin, district Alt-Tegel
 modelData = [
     {
         name: "Bergmann Pizza",
         yelpID: "bergmann-pizza-berlin-2",
         category: "food",
-        fsquareID: "4ece1682e3007feb7ad1bd94"
+        fsquareID: "4ece1682e3007feb7ad1bd94",
+        lat: 52.589969,
+        lng: 13.281440
     },
     {
         name: "L'Angolo del Gelato",
         yelpID: "l-angolo-del-gelato-berlin-2",
         category: "food",
-        fsquareID: "4e89c3e1d22d940664f12faf"
+        fsquareID: "4e89c3e1d22d940664f12faf",
+        lat: 52.589428,
+        lng: 13.281779
     },
     {
-        name: "Pharmacy: Alte Spree Apotheke",
+        name: "Alte Spree Apotheke",
         yelpID: "alte-spree-apotheke-berlin",
         category: "other",
-        fsquareID: "532d9a8e498ec8516363d00d"
+        fsquareID: "532d9a8e498ec8516363d00d",
+        lat: 52.592183,
+        lng: 13.282605
     },
     {
         name: "Borsighallen",
         yelpID: "hallen-am-borsigturm-berlin?osq=borsighallen",
         category: "shopping",
-        fsquareID: "4b8d374ff964a520deed32e3"
+        fsquareID: "4b8d374ff964a520deed32e3",
+        lat: 52.584968,
+        lng: 13.286043
     },
     {
         name: "Lucky Chinese",
         yelpID: "lucky-chinese-berlin",
         category: "food",
-        fsquareID: "4cfb87c0feec6dcb2b314736"
+        fsquareID: "4cfb87c0feec6dcb2b314736",
+        lat: 52.588512,
+        lng: 13.278548
+    },
+    {
+        name: "R\u00fcan-Thai",
+        category: "food",
+        lat: 52.588118,
+        lng: 13.286179
+    },
+    {
+        name: "Schloss-Apotheke Tegel",
+        category: "other",
+        lat: 52.588731,
+        lng: 13.284178
+    },
+    {
+        name: "Intersport Voswinkel",
+        category: "shopping",
+        lat: 52.584321,
+        lng:  13.286233
+    },
+    {
+        name: "Holi",
+        category: "food",
+        lat: 52.586944,
+        lng: 13.281101
     }
 ];
 
 
+// foursquare credentials
+var clientID = "15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5PA2FOZNPHLTK";
+var clientSecret = "PPCH5JDLNLMPTFBWFBEUO4GOO3B4HIVNZ0IRYJMLRNGTWVHK&v=20170801";
+
+
+// the model, which defines information and functions on our model-data
 var model = function(modelData) {
     var self = this;
     this.name = modelData.name;
     this.id = modelData.fsquareID;
-    this.category = modelData.category;
+    this.category = modelData.category;  // one of three categories (food, shopping, other)
     this.visible = ko.observable();
-    this.URL = '';
+    this.lat = modelData.lat;
+    this.lng = modelData.lng;
     this.street = '';
     this.city = '';
-    this.hours = '';
-    this.rating = '';
+    this.openingTimes = '';  // Contains the opening hours of this point of interest
+    this.rating = '';  // Numerical rating of the venue (0 through 10)
+    this.cat = '';  // more precise category (e.g. Pizza Place)
 
-    var fsquareURL = 'https://api.foursquare.com/v2/venues/' + this.id + '&client_id=15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5PA2FOZNPHLTK&client_secret=PPCH5JDLNLMPTFBWFBEUO4GOO3B4HIVNZ0IRYJMLRNGTWVHK';
+    // API endpoint of forsquare, check out: https://developer.foursquare.com/docs/venues/search
+    // OAuth doesn't allow CORS, so one can't directly fetch location info via the forsquareID, see: https://developer.foursquare.com/docs/venues/venues
+    // since we want to only use frontend for this project
+    var fsquareURL =  'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.lng + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20170804' + '&query=' + this.name + '&limit=1';
 
+    // ajax call to get information on point of interest
+    // response: https://developer.foursquare.com/docs/responses/venue
     $.ajax({
         type: 'GET',
-        url: 'fsquareURL',
+        url: fsquareURL,
         dataType: 'json',
-        success: function(response) {
-            fsquare = response.response;
-            console.log("getJSON success");
-            window.fsquare = fsquare;
+        success: function(data) {
+            fsquare = data.response.venues[0];
+            console.log("getJSON success: " + self.name);
+            self.street = fsquare.location.address;
+            self.city = fsquare.location.city;
+            self.openingTimes = fsquare.hours;
+            self.rating = fsquare.rating;
+            self.cat = fsquare.categories[0].shortName;
+            // some values might be undefined if not provided, re-set those to empty string
+            if (typeof self.street == 'undefined') { self.street = '<i>unknown</i>'; }
+            if (typeof self.city == 'undefined') { self.city = ''; }
+            if (typeof self.openingTimes == 'undefined') { self.openingTimes = '<i>not available..</i>'; }
+            if (typeof self.rating == 'undefined') { self.rating = '<i>not yet rated...</i>'; }
+            if (typeof self.cat == 'undefined') { self.cat = ''; }
+
+            // generate info-windows for each marker, following the documentation
+            // https://developers.google.com/maps/documentation/javascript/infowindows
+            // with improved string-concenation (just for fun)
+            var contentString = [];
+            contentString.push('<div class="contentString">',
+                                  '<p><b>', self.name, '</b>',
+                                  '<br><i>', self.cat, '</i></p>',
+                                  '<p>', self.street, '<br>', self.city, '</p>',
+                                  '<p>Opening hours:<br>', self.openingTimes, '</p>',
+                                  '<p>User rating (0-10): ', self.rating, '</p>',
+                               '</div>');
+            self.infowindow = new google.maps.InfoWindow({
+                content: contentString.join('')
+            });
+            self.marker.addListener('click', function() {
+                self.infowindow.open(map, self.marker);
+            });
+        },
+        fail: function() {
+            alert("Sorry, we are having problems fetching information from foursquare. Pleasy try refreshing your browswer page.");
         }
     });
 
@@ -129,19 +207,50 @@ var model = function(modelData) {
     // marker shall be opened
     this.showInfo = function() {
         console.log("you clicked " + self.name);
+        console.log("id: " + self.id);
+        console.log("category: " + self.category);
+        console.log("street: " + self.street);
+        console.log("city: " + self.city);
+        console.log("opening times: " + self.openingTimes);
+        console.log("rating: " + self.rating);
+        console.log("cat: " + self.cat);
+        self.infowindow.open(map, self.marker);
     }
 
+    // generate map-markers
+    // custom icons: https://sites.google.com/site/gmapsdevelopment/
+    switch (self.category) {
+        case "food":
+            var iconLink = 'http://maps.google.com/mapfiles//kml/pal2/icon32.png';
+            break;
+        case "shopping":
+            var iconLink = 'http://maps.google.com/mapfiles//kml/pal3/icon26.png';
+            break;
+        case "other":
+            var iconLink = 'http://maps.google.com/mapfiles//kml/pal3/icon53.png';
+            break;
+    }
+    this.marker = new google.maps.Marker({
+        position: {lat: this.lat, lng: this.lng},
+        title: this.name,  // appears as tooltip
+        icon: iconLink
+    });
+    // add marker to the map (necessary if not called within initMap)
+    this.marker.setMap(map);
 }
+
 
 function viewModel() {
     var self = this;  // use self inside the viewModel for clear distinguishing
     this.Query = ko.observable('');
     this.itemlist = ko.observableArray([]);
 
+    // init google maps
+    initMap();
+
     // populate the itemlist with all items from our model-data
     modelData.forEach(function(item) {
         self.itemlist.push(new model(item));
-        console.log(self.name);
     });
 
     // generate list which results from search-input
@@ -156,4 +265,9 @@ function viewModel() {
 
 }
 
-ko.applyBindings(new viewModel());
+// "Uncaught ReferenceError: google is not defined" @ "this.marker = new google.maps.Marker({.." inside
+// "var model = function" because that used to run bevore the map is loaded, so we have to run all application code
+// after the map is loaded
+function runApp() {
+    ko.applyBindings(new viewModel());
+}
--- js/foursquare_token.js
+++ js/foursquare_token.js
@@ -5,6 +5,7 @@ link: https://foursquare.com/developers/app/15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5
 helpful links:
 https://developer.foursquare.com/overview/auth#userless
 https://developer.foursquare.com/docs/venues/venues
+https://developer.foursquare.com/overview/versioning
 
 CLIENT_ID: 15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5PA2FOZNPHLTK
 
@@ -12,11 +13,21 @@ CLIENT_SECRET: PPCH5JDLNLMPTFBWFBEUO4GOO3B4HIVNZ0IRYJMLRNGTWVHK
 
 
 GET-Request to:
-https://api.foursquare.com/v2/venues/VENUE_ID&client_id=---&client_secret=---
+https://api.foursquare.com/v2/venues/VENUE_ID&client_id=...&client_secret=...&v=...&m=...
 for copy-pasta:
 https://api.foursquare.com/v2/venues/VENUE_ID&client_id=15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5PA2FOZNPHLTK&client_secret=PPCH5JDLNLMPTFBWFBEUO4GOO3B4HIVNZ0IRYJMLRNGTWVHK
 
 Bergmann Pizza:
 https://api.foursquare.com/v2/venues/4ece1682e3007feb7ad1bd94&client_id=15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5PA2FOZNPHLTK&client_secret=PPCH5JDLNLMPTFBWFBEUO4GOO3B4HIVNZ0IRYJMLRNGTWVHK
+
+
+
+
+https://api.foursquare.com/v2/venues/4ece1682e3007feb7ad1bd94&client_id=15S4QDKN4EJG4MNX2XPXUSLBWB3ADMHJYJY5PA2FOZNPHLTK&client_secret=PPCH5JDLNLMPTFBWFBEUO4GOO3B4HIVNZ0IRYJMLRNGTWVHK&v=20170801
+
+
+
+
+
 
 */
